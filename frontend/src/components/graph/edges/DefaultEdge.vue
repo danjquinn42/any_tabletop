53d50e3074e24a22a8f206c478b8a15169bdb4ea
danjquinn42 <danjquinn42@gmail.com>
2024-08-08 09:59:17 -0400

    Install vue-flow and test making simple graph connections

---
+++ frontend/src/components/graph/edges/DefaultEdge.vue
@@ -0,0 +1,35 @@
+<template>
+  <path
+      :d="pathData"
+      stroke="black"
+      stroke-width="2"
+      fill="none"
+      @click="onClick"
+  />
+</template>
+
+<script>
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    data: Object,
+    sourcePosition: Object,
+    targetPosition: Object,
+  },
+  setup(props) {
+    const pathData = computed(() => {
+      // Compute the path data for the edge
+      const { sourcePosition, targetPosition } = props;
+      return `M${sourcePosition.x},${sourcePosition.y} L${targetPosition.x},${targetPosition.y}`;
+    });
+
+    const onClick = () => {
+      // Handle click event to edit or show edge details
+      console.log('Edge clicked:', props.data);
+    };
+
+    return { pathData, onClick };
+  },
+});
+</script>
